c3c3b4f8a75b0826af3b21a29bcde9734b42d11d
ayatarefin <ayatullaharefin@gmail.com>
2024-10-20 17:52:17 +0600

    watch

---
+++ script.js
@@ -0,0 +1,41 @@
+//digital clock part
+
+function digitalClock(){
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours >=12? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const seconds = now.getSeconds().toString().padStart(2,0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
+}
+
+digitalClock();
+setInterval(digitalClock,1000);
+
+
+// for analog
+
+var inc = 1000;
+
+clock();
+
+function clock() {
+  const date = new Date();
+
+  const hours = ((date.getHours() + 11) % 12 + 1);
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  
+  const hour = hours * 30;
+  const minute = minutes * 6;
+  const second = seconds * 6;
+  
+  document.querySelector('.hour').style.transform = `rotate(${hour}deg)`
+  document.querySelector('.minute').style.transform = `rotate(${minute}deg)`
+  document.querySelector('.second').style.transform = `rotate(${second}deg)`
+}
+
+setInterval(clock, inc);
